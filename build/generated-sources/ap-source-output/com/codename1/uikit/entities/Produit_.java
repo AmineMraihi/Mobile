6873a1c96ac59978a074271d71e6e4104cc28ea3
Unknown <amine.mraihi@esprit.tn>
2018-02-12 22:00:13 +0100

    final

--- build/generated-sources/ap-source-output/com/codename1/uikit/entities/Produit_.java
+++ build/generated-sources/ap-source-output/com/codename1/uikit/entities/Produit_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-10T12:50:55")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-10T15:09:07")
 @StaticMetamodel(Produit.class)
 public class Produit_ { 
 
